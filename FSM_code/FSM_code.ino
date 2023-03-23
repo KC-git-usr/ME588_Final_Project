a3d9d1626666df6a694543c2ca229fea9134e10b
Kumar Ramesh Chakravarthy <69975720+KC-git-usr@users.noreply.github.com>
2023-03-22 23:47:25 -0400

    March 22, 9 PM FSM

--- FSM_code/FSM_code.ino
+++ FSM_code/FSM_code.ino
@@ -1,34 +1,56 @@
 /*
   ME 588 Final Project
-*/
+  Team 10
+
+  Reference: https://docs.google.com/presentation/d/1SksHOphqhfJFn6xp7ug-bYm4VV44OZiPOhLUAs5DCw4/edit?usp=sharing
+  This program will implement our core FSM logic and
+  will be the base program that makes other custom
+  functions calls
+
+  TO DO:
+  Verify timer of 2 min
+  Verify data type for pins
+  Mimic pause time if e-stopped and resumed?
+
+  Author:
+  Kumar Ramesh (ramesh64@purdue.edu)
+  22nd Mar, 2023
+ */
 
 // Defining pin numbers
-const int CS_pin = 2;
-const int HS_pin = 3;
-const int SG_pin = 4;
-const int SP_pin = 5;
-const int HP_pin = 6;
-const int CD_pin = 7;
+const char SG_pin = 2;  // Start Game pin
+const char ES_pin = 3;  // Emergency Stop pin
+const char LD_pin = 4;  // Line Detected pin
+const char EC_pin = 5;  // Encoder Count pin
+const char US_pin = 6;  // Ultra-Sonic sensor pin
 
 // Defining FSM input variables
-int CS = 0; // Cube sensor
-int HS = 0; // Home Square
-int SG = 0; // Start Game
+boolean SG = 0; // Start Game
+boolean ES = 0; // Emergency Stop
+boolean LD = 0; // Line Detected
+boolean EC = 0; // Encoder Count
+boolean US = 0; // Ultra-Sonic sensor
 
 // Defining FSM output variables
-int SP_state = 0; // Search pattern
-int HP_state = 0; // Home pattern
-int CD_state = 0; // Cube delivery
+boolean FL = 0; // Flaps
+boolean MF = 0; // Move Forward
+boolean RT = 0; // Right Turn
 
 // Defining states for FSM
-const int STATE_0 = 0; // START
-const int STATE_1 = 1; // SEARCH
-const int STATE_2 = 2; // DELIVER
-const int STATE_3 = 3; // GO HOME
-const int STATE_4 = 4; // STOP
+const byte STATE_0 = 0; // STOP
+const byte STATE_1 = 1; // FORWARD
+const byte STATE_2 = 2; // RIGHT TURN
+const byte STATE_3 = 3; // EMERGENCY STOP
 
-// Initialiing the first state of the robot
+// Defining time limit
+const unsigned long TIME_LIMIT = 120000; // [ms]
+
+// Initializing the first state of the robot
 int FSM_state = STATE_0;
+
+// Variable to hold the 2min timer
+unsigned long time_since_prgm_start = 0;
+unsigned long time_since_SG = 0;
 
 // Variable to read virtual input from keyboard
 String input_data;
@@ -52,80 +74,63 @@ void loop() {
   // Get virtual input from user
   GetInputData();
 
+  // Function to determine value of variable SG
+  time_since_prgm_start = millis();
+  CheckTime();
+
   switch(FSM_state) {
     case  STATE_0:
       // perform action
       StayStationary();
-      HoldCubes();
-      // transistion logic
-      if((CS == 1) && (HS == 0) && (SG == 1)) {
-        FSM_state = STATE_3;
-      } else if((CS == 0) && (HS == 0) && (SG == 1)) {
-        FSM_state = STATE_1;        
-      } else if(SG == 0) {
-        FSM_state = STATE_4;        
-      } else if((HS == 1) && (SG == 1)) {
-        FSM_state = STATE_2;        
+      RetractFlaps();
+      // transition logic
+      if(SG == 0) {
+          FSM_state = STATE_0;
+      } else if(SG == 1){
+          StartTimer();
+          FSM_state = STATE_1;
       }
     break;
 
     case  STATE_1:
       // perform action
-      ActivateSearchPattern();
-      HoldCubes();
-      // transistion logic
-      if((CS == 0) && (HS == 0) && (SG == 1)) {
-        FSM_state = STATE_1;        
-      } else if(SG == 0) {
-        FSM_state = STATE_4;        
-      } else if((CS == 1) && (HS == 0) && (SG == 1)) {
-        FSM_state = STATE_3;        
-      } else if((HS == 1) && (SG == 1)) {
-        FSM_state = STATE_2;        
+      DeployFlaps();
+      MoveForward();
+      // transition logic
+      if((SG == 1) && (ES ==0) && (EC == 0) && (US == 0)) {
+        FSM_state = STATE_1;
+      } else if((SG == 1) && (ES == 0) && (EC == 1) && (US == 1)) {
+        StayStationary();  // Stop moving before turning right
+        FSM_state = STATE_2;
+      } else if(ES == 1) {
+        FSM_state = STATE_3;
+      } else if(((SG == 1) && (LD == 1)) || (SG == 0)) {
+        FSM_state = STATE_0;
       }
     break;
 
     case  STATE_2:
       // perform action
-      StayStationary();
-      ReleaseCubes();
-      // transistion logic
-      if((CS == 0) && (SG == 1)) {
-        FSM_state = STATE_1;        
-      } else if(SG == 0) {
-        FSM_state = STATE_4;        
-      } else if((CS == 1) && (HS == 0) && (SG == 1)) {
-        FSM_state = STATE_3;        
-      } else if((CS == 1) && (HS == 1) && (SG == 1)) {
-        FSM_state = STATE_2;        
+      DeployFlaps();
+      TurnRight();
+      // transition logic
+      if((SG == 1) && (ES == 0) && (EC == 0) && (US == 1)) {
+        FSM_state = STATE_2;
+      } else if(ES == 1) {
+        FSM_state = STATE_3;
       }
     break;
 
     case  STATE_3:
       // perform action
-      ActivateHomePattern();
-      HoldCubes();
-      // transistion logic
-      if(SG == 0) {
-        FSM_state = STATE_4; 
-      } else if((CS == 0) && (HS == 0) && (SG == 1)) {
-        FSM_state = STATE_1;
-      } else if((CS == 1) && (HS == 0) && (SG == 1)) {
-        FSM_state = STATE_3;
-      } else if((HS == 1) && (SG == 1)) {
-        FSM_state = STATE_2;
-      }
-    break;
-
-    case  STATE_4:
-      // perform action
       StayStationary();
-      HoldCubes();
-      // transistion logic
-      if(SG == 1) {
+      // transition logic
+      if((SG == 1) && (ES == 0)) {
         FSM_state = STATE_1;
-      } else if(SG == 0) {
-        FSM_state = STATE_4;
+      } else if(ES == 1) {
+        FSM_state = STATE_3;
+      } else if((SG == 0) && (ES == 0)) {
+          FSM_state = STATE_0;
       }
     break;    
   }
@@ -138,28 +143,59 @@ void loop() {
 void PrintStatus() {
   // Simply print input data and FSM status
   Serial.println("----------------------------------");
-  Serial.print("Current input (CS, HS, SG) : "); Serial.print(CS); Serial.print(HS); Serial.println(SG);
+  Serial.print("Current input (SG, ES, LD, EC, US) : "); Serial.print(SG); Serial.print(ES); Serial.print(LD);
+  Serial.print(EC); Serial.println(US);
   Serial.print("FSM_state : "); Serial.println(FSM_state);
+  Serial.print("Time : ");
+  if(SG)
+    Serial.println(time_since_prgm_start - time_since_SG);
+  else
+    Serial.println(0);
   Serial.println();
 }
 
 
 void GetInputData() {
   /*
-    Read all 3 inputs at once
-    Example : Enter 101 and press enter
-    This will set CS = 1, HS = 0, SG = 1
-    NOTE : enter a 3 digit num, with 1's and 0's only
+    Read all 5 inputs at once
+    Example : Enter 10101 and press enter
+    This will set SG = 1, ES = 0, LD = 1, EC = 0, US = 1
+    NOTE : enter a 5 digit num, with 1's and 0's only
   */
   if(Serial.available() > 0) {
     input_data = Serial.readString();
     input_data.trim();
-    SG = input_data[2] == '1' ? 1 : 0;
-    HS = input_data[1] == '1' ? 1 : 0;
-    CS = input_data[0] == '1' ? 1 : 0;
+    SG = input_data[0] == '1' ? 1 : 0;
+    ES = input_data[1] == '1' ? 1 : 0;
+    LD = input_data[2] == '1' ? 1 : 0;
+    EC = input_data[3] == '1' ? 1 : 0;
+    US = input_data[4] == '1' ? 1 : 0;
     Serial.print("Received : "); Serial.println(input_data);
     // Serial.flush();
   }
+}
+
+
+void StartTimer() {
+  /*
+    Function to start our game timer
+  */
+  time_since_SG = millis(); // record curr time to use as reference zero
+}
+
+
+void CheckTime() {
+  /*
+    Function to determine value of variable SG
+  */
+    if((time_since_SG > 0) && ((time_since_prgm_start - time_since_SG) >= TIME_LIMIT)) { // exceeding 2 min limit
+        SG = 0;
+        Serial.println("2 min UP!");
+    } else if((time_since_SG == 0) && (SG == 0)){ // timer not started yet and input SG is still 0
+        SG = 0;
+    } else { // game started
+        SG = 1;
+    }
 }
 
 
@@ -167,43 +203,68 @@ void StayStationary() {
   /*
     Logic to keep the robot stationary
   */  
-  SP_state = 0;
-  HP_state = 0;
-  // add logic here  
+  if(MF || RT) { // robot not stationary
+      Serial.println("Stopping robot motion");
+      // add logic here
+      MF = 0;
+      RT = 0;
+  } else {
+      Serial.println("Robot is already stationary");
+  }
 }
 
 
-void HoldCubes() {
-  /*
-    Logic to hold the cubes
-  */  
-  CD_state = 0;
-  // add logic here   
+void MoveForward() {
+    /*
+      Logic to keep the robot moving in a straight line
+    */
+    if(MF) { // robot moving forward
+        Serial.println("Robot is already moving forward");
+    } else {
+        Serial.println("Making robot move forward");
+        // add logic here
+        MF = 1;
+    }
 }
 
 
-void ActivateSearchPattern() {
-  /*
-    Logic to start search pattern
-  */  
-  SP_state = 1;
-  // add logic here   
+void TurnRight() {
+    /*
+      Logic to make the robot take a 90 deg right turn
+    */
+    if(RT) { // robot turning right
+        Serial.println("Robot is already turning right");
+    } else {
+        Serial.println("Making robot turn right");
+        // add logic here
+        RT = 1;
+    }
 }
 
 
-void ActivateHomePattern() {
+void DeployFlaps() {
   /*
-    Logic to start home drive
+    Logic to deploy flaps
   */  
-  HP_state = 1;
-  // add logic here  
+  if(FL) {  // flaps deployed
+      Serial.println("Flaps are already deployed");
+  } else {
+      Serial.println("Deploying flaps");
+      // add logic here
+      FL = 1;
+  }
 }
 
 
-void ReleaseCubes() {
+void RetractFlaps() {
   /*
-    Logic to release cubes
-  */  
-  CD_state = 1;
-  // add logic here   
+    Logic to retract flaps
+  */
+  if(!FL) {  // flaps retracted
+    Serial.println("Flaps are already retracted");
+  } else {
+    Serial.println("Retracting flaps");
+    // add logic here
+    FL = 0;
+  }
 }
