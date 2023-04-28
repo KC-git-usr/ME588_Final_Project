4c0d0347b7c1f2fa721982a796a63a05933d4b62
KumanshF <kumansh.furia@gmail.com>
2023-04-28 18:12:52 -0400

    Update WallFollower.cpp
    Update

--- Wall_Follower/WallFollower/WallFollower.cpp
+++ Wall_Follower/WallFollower/WallFollower.cpp
@@ -3,7 +3,7 @@
 #define DEBUG true
 
 // Defining time limit
-const unsigned long TIME_LIMIT = 120000; // [ms]
+const unsigned long TIME_LIMIT = 90000; // [ms]
 
 // Variable to hold the 2min timer
 unsigned long time_since_prgm_start = 0;
@@ -12,10 +12,15 @@ unsigned long time_at_SG = 0;
 // Maximum distance for the ultrasonic sensors (in cm)
 const int maxDistance = 300;
 
-//Moving average array
-int movingAvg[] = {0,0,0,0,0};
-int avgIndx = 0;
-int getFrontDist();
+//
+const int frontWallDetectDistance = 35;
+
+//LF stuff
+int l_threshold_ = 500;
+int r_threshold_ = 500;
+static int counter = 0;
+static int prev_lf_rdg_1 = 0;
+static int prev_lf_rdg_2 = 0;
 
 // Button
 const int buttonPins[] = {35, 37};
@@ -25,13 +30,13 @@ uint8_t testCount[WallFollower::noOfButtons];
 
 // Initialize the ultrasonic sensors
 NewPing frontSensor(WallFollower::frontTrigPin, WallFollower::frontEchoPin, maxDistance);
-NewPing leftSensor(WallFollower::leftTrigPin, WallFollower::leftEchoPin, maxDistance);
-NewPing midSensor(WallFollower::midTrigPin, WallFollower::midEchoPin, 80);
+NewPing leftSensor(WallFollower::leftTrigPin, WallFollower::leftEchoPin, 50);
+NewPing midSensor(WallFollower::midTrigPin, WallFollower::midEchoPin, 40);
 
 // PID controller parameters
-const float Kp = 0.7;
-const float Ki = 0.01;
-const float Kd = 0.2;
+const float Kp = 0.8;
+const float Ki = 0.02;
+const float Kd = 0.08;
 // const float Kp = 1.2;
 // const float Ki = 0.00;
 // const float Kd = 0.00;
@@ -39,8 +44,10 @@ float integral = 0;
 float previousError = 0;
 unsigned long previousTime = 0;
 
+int baseSpeed = 75;
+
 // Desired distance from the left wall (in cm)
-const int desiredDistance = 27;
+const int desiredDistance = 18;
 bool startGame = false;
 char colors[] = {'Y', 'G', 'B', 'R'};
 uint8_t color = 0;  // {1, Y}, {2, G}, {3, B}, {4, R}
@@ -56,6 +63,7 @@ void WallFollower::setup() {
     pinMode(WallFollower::motor2In1, OUTPUT);
     pinMode(WallFollower::motor2In2, OUTPUT);
     pinMode(WallFollower::motor2Pwm, OUTPUT);
+    pinMode(WallFollower::irsensor, INPUT);
 
     //button Setup
     for (int i = 0; i < WallFollower::noOfButtons; ++i) {
@@ -72,6 +80,12 @@ void WallFollower::setup() {
 void debounce();
 void CheckTime();
 void StartTimer();
+void getLF_Rdg();
+
+long frontDistance = 0;
+long midDistance = 0;
+long leftDistance = 0;
+int irsensor_reading = 0;
 
 void WallFollower::loop() {
 
@@ -79,20 +93,30 @@ void WallFollower::loop() {
     time_since_prgm_start = millis();
     CheckTime();
 
-    //get button presses
-    debounce();
-
     if (startGame == false){
+        //get button presses
+        debounce();
         lcd.setCursor(0,1);
         lcd.print(colors[color]);
     }
 
+    
+
     if(startGame==true){
+        // get LF reading
+        // getLF_Rdg();
+        // if(counter == 3) {
+        //   startGame = false;
+        //   if(DEBUG) {
+        //       Serial.println("Reached home base");
+        //   }
+        // }
 
         // Read the distance from the ultrasonic sensors
-        long frontDistance = frontSensor.ping_cm();
-        long midDistance = midSensor.ping_cm();
-        long leftDistance = leftSensor.ping_cm();
+        frontDistance = frontSensor.ping_cm();
+        midDistance = midSensor.ping_cm();
+        leftDistance = leftSensor.ping_cm();
+        irsensor_reading = digitalRead(WallFollower::irsensor);
         
         // Calculate the time since the last loop iteration
         unsigned long currentTime = millis();
@@ -106,16 +130,71 @@ void WallFollower::loop() {
         float output = Kp * error + Ki * integral + Kd * derivative;
         previousError = error;
 
-        // Control the motors based on the PID output
-        if (frontDistance > 30) {
+        if((frontDistance <= frontWallDetectDistance) || (midDistance < 10 && midDistance > 0)) {
+                        
+            // Stop
+            digitalWrite(WallFollower::motor1In1, LOW);
+            digitalWrite(WallFollower::motor1In2, LOW);
+            analogWrite(WallFollower::motor1Pwm, 0);
+            digitalWrite(WallFollower::motor2In1, LOW);
+            digitalWrite(WallFollower::motor2In2, LOW);
+            analogWrite(WallFollower::motor2Pwm, 0);
+
+            delay(10);
+
+            digitalWrite(WallFollower::motor1In1, HIGH);
+            digitalWrite(WallFollower::motor1In2, LOW);
+            analogWrite(WallFollower::motor1Pwm, 75);
+            digitalWrite(WallFollower::motor2In1, LOW);
+            digitalWrite(WallFollower::motor2In2, HIGH);
+            analogWrite(WallFollower::motor2Pwm, 75);
+
+            delay(200);
+
+            // Stop
+            // Serial.println("Stoping");
+            digitalWrite(WallFollower::motor1In1, LOW);
+            digitalWrite(WallFollower::motor1In2, LOW);
+            analogWrite(WallFollower::motor1Pwm, 0);
+            digitalWrite(WallFollower::motor2In1, LOW);
+            digitalWrite(WallFollower::motor2In2, LOW);
+            analogWrite(WallFollower::motor2Pwm, 0);
+
+            delay(10);
+
+            // Update the LCD display
+            lcd.setCursor(0,0);
+            lcd.print("S: ");
+            lcd.print("      ");
+            lcd.setCursor(2,0);
+            lcd.print("Turn");
+            lcd.setCursor(0,1);
+            lcd.print(counter);
+            // lcd.setCursor(0,1);
+            // lcd.print("F:");
+            // lcd.print("    ");
+            // lcd.setCursor(2, 1);
+            // lcd.print(frontDistance);
+            // lcd.setCursor(7,1);
+            // lcd.print("M:");
+            // lcd.print("    ");
+            // lcd.setCursor(9, 1);
+            // lcd.print(midDistance);
+            // lcd.setCursor(13,1);
+            // lcd.print("L:");
+            // lcd.print("    ");
+            // lcd.setCursor(15, 1);
+            // lcd.print(leftDistance);
+
+        } else { // Control the motors based on the PID output
+            // if ((frontDistance > 30) && (midDistance == 0)) {
             // Move forward and adjust the speed based on the PID output
-            int baseSpeed = 80;
             int motor1Speed = baseSpeed + output;
             int motor2Speed = baseSpeed - output;
 
             // Constrain the motor speeds
-            motor1Speed = constrain(motor1Speed, 0, 100);
-            motor2Speed = constrain(motor2Speed, 0, 100);
+            motor1Speed = constrain(motor1Speed, 0, 120);
+            motor2Speed = constrain(motor2Speed, 0, 120);
 
             // Set the motor speeds and directions
             // Serial.println("Moving forward");
@@ -129,110 +208,30 @@ void WallFollower::loop() {
             // Update the LCD display
             lcd.setCursor(0,0);
             lcd.print("S: ");
-            lcd.print("Moving Forward");
-             lcd.setCursor(0,1);
-            lcd.print("F:");
-            lcd.print("    ");
-            lcd.setCursor(2, 1);
-            lcd.print(frontDistance);
-            lcd.setCursor(7,1);
-            lcd.print("M:");
-            lcd.print("    ");
-            lcd.setCursor(9, 1);
-            lcd.print(midDistance);
-            lcd.setCursor(13,1);
-            lcd.print("L:");
-            lcd.print("    ");
-            lcd.setCursor(15, 1);
-            lcd.print(leftDistance);
-
-        } 
-        else if(frontDistance <= 30 ) {
-            // Update the LCD display
-            lcd.setCursor(0,0);
-            lcd.print("S: ");
-            lcd.print("               ");
-            lcd.setCursor(3,0);
-            lcd.print("Turning");
+            lcd.print("      ");
+            lcd.setCursor(2,0);
+            lcd.print("Move");
             lcd.setCursor(0,1);
-            lcd.print("F:");
-            lcd.print("    ");
-            lcd.setCursor(2, 1);
-            lcd.print(frontDistance);
-            lcd.setCursor(7,1);
-            lcd.print("M:");
-            lcd.print("    ");
-            lcd.setCursor(9, 1);
-            lcd.print(midDistance);
-            lcd.setCursor(13,1);
-            lcd.print("L:");
-            lcd.print("    ");
-            lcd.setCursor(15, 1);
-            lcd.print(leftDistance);
-            
-            // Stop
-            digitalWrite(WallFollower::motor1In1, LOW);
-            digitalWrite(WallFollower::motor1In2, LOW);
-            analogWrite(WallFollower::motor1Pwm, 0);
-            digitalWrite(WallFollower::motor2In1, LOW);
-            digitalWrite(WallFollower::motor2In2, LOW);
-            analogWrite(WallFollower::motor2Pwm, 0);
+            lcd.print(counter);
+            // lcd.setCursor(0,1);
+            // lcd.print("F:");
+            // lcd.print("    ");
+            // lcd.setCursor(2, 1);
+            // lcd.print(frontDistance);
+            // lcd.setCursor(6,1);
+            // lcd.print("M:");
+            // lcd.print("    ");
+            // lcd.setCursor(8, 1);
+            // lcd.print(midDistance);
+            // lcd.setCursor(11,1);
+            // lcd.print("L:");
+            // lcd.print("    ");
+            // lcd.setCursor(13, 1);
+            // lcd.print(leftDistance);
 
-            delay(500);
+        }  
 
-            //Turn
-            while(frontDistance < 30 || (midDistance < 18 && midDistance > 0)){
-                digitalWrite(WallFollower::motor1In1, HIGH);
-                digitalWrite(WallFollower::motor1In2, LOW);
-                analogWrite(WallFollower::motor1Pwm, 80);
-                digitalWrite(WallFollower::motor2In1, LOW);
-                digitalWrite(WallFollower::motor2In2, HIGH);
-                analogWrite(WallFollower::motor2Pwm, 80);
-
-                frontDistance = frontSensor.ping_cm();
-                midDistance = midSensor.ping_cm();
-
-                if (DEBUG) {
-                    Serial.print("Turning   ");
-                    Serial.print(frontDistance);
-                    Serial.print("  ");
-                    Serial.println(midDistance);
-                }
-
-                lcd.setCursor(0,0);
-                lcd.print("S: ");
-                lcd.print("               ");
-                lcd.setCursor(3,0);
-                lcd.print("Turning");
-                lcd.setCursor(0,1);
-                lcd.print("F:");
-                lcd.print("    ");
-                lcd.setCursor(2, 1);
-                lcd.print(frontDistance);
-                lcd.setCursor(7,1);
-                lcd.print("M:");
-                lcd.print("    ");
-                lcd.setCursor(9, 1);
-                lcd.print(midDistance);
-                lcd.setCursor(13,1);
-                lcd.print("L:");
-                lcd.print("    ");
-                lcd.setCursor(15, 1);
-                lcd.print(leftDistance);
-            }
-
-            // Stop
-            Serial.println("Stoping");
-            digitalWrite(WallFollower::motor1In1, LOW);
-            digitalWrite(WallFollower::motor1In2, LOW);
-            analogWrite(WallFollower::motor1Pwm, 0);
-            digitalWrite(WallFollower::motor2In1, LOW);
-            digitalWrite(WallFollower::motor2In2, LOW);
-            analogWrite(WallFollower::motor2Pwm, 0);
-
-        }
-
-        if (DEBUG == true) {
+        if (DEBUG) {
             Serial.print("FD: ");
             Serial.print(frontDistance);
             Serial.print(" MD: ");
@@ -240,7 +239,9 @@ void WallFollower::loop() {
             Serial.print(" LD: ");
             Serial.print(leftDistance);
             Serial.print(" O: ");
-            Serial.println(output);
+            Serial.print(output);
+            Serial.print("  IR: ");
+            Serial.println(irsensor_reading);
         }    
     }
 }
@@ -288,15 +289,33 @@ void CheckTime() {
         startGame = false;
 //        lcd.clear();
 //        lcd.print("TIME UP!!!");
-        Serial.println("TIME UP!!!");
+        Serial.print("TIME UP!!!  "); Serial.println(startGame);
     } else if((time_at_SG == 0) && (startGame == false)){ // timer not started yet and input SG is still 0
         startGame = false;
     }
 }
 
-// int getFrontDist(){
-//   long frontDist = frontSensor.ping_cm();
-//   movingAvg[avgIndx++] = frontDist;
-//   if(avgIndx == 5) {avgIndx = 0;}
-//   return (int((movingAvg[0] + movingAvg[1] + movingAvg[2]+ movingAvg[3] + movingAvg[4]) / 5));
-// }
+void getLF_Rdg() {
+
+    // int curr_lf_rdg_1 = analogRead(WallFollower::LF1);
+    int curr_lf_rdg_2 = analogRead(WallFollower::LF2);
+
+    if(DEBUG) {
+        // Serial.print("LF 1 value:  ");
+        // Serial.print(curr_lf_rdg_1);
+        Serial.print("  LF 2 value:  ");
+        Serial.println(curr_lf_rdg_2);
+    }
+
+    // CHANGE THESE THRESHOLD VALUES
+    // if (((prev_lf_rdg_1 > l_threshold_) && (prev_lf_rdg_2 > r_threshold_)) &&
+    // ((curr_lf_rdg_1 < l_threshold_) && (curr_lf_rdg_2 < r_threshold_)))  //tune threshold for LF
+    //     counter++;
+
+    if ((prev_lf_rdg_2 < r_threshold_) && (curr_lf_rdg_2 > r_threshold_))  //tune threshold for LF
+        counter++;
+        
+
+    // prev_lf_rdg_1 = curr_lf_rdg_1;
+    prev_lf_rdg_2 = curr_lf_rdg_2;
+};
